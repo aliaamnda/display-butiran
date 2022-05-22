78f2edadf5daf16bdd292294f625b96e6bf05e47
aliaamnda <103908169+aliaamnda@users.noreply.github.com>
2022-05-23 01:13:30 +0800

    test

--- butiran.js
+++ butiran.js
@@ -5,8 +5,44 @@ function borang() {
     document.getElementById("paparalamat").innerHTML = alamat;
     let bangsa = document.getElementById("bangsa").value;
     document.getElementById("paparbangsa").innerHTML = bangsa;
-    jantina = 'try jantina';
-    let jantina = document.getElementById("jantina").value;
-    document.getElementById("paparjantina").innerHTML = jantina;
     return false;
+}
+
+function checkButton() {
+    if (document.getElementById('lelaki').checked) {
+        document.getElementById("disp").innerHTML
+            = document.getElementById("lelaki").value;
+    }
+    else if (document.getElementById('wanita').checked) {
+        document.getElementById("disp").innerHTML
+            = document.getElementById("wanita").value;
+    }
+    else {
+        document.getElementById("error").innerHTML
+        = "Sila pilih jantina";
+    }
+}
+
+function borang() {
+    let x = document.forms["myForms"]["nama"].value;
+    if(x=="") {
+        alert("Name must be filled out");
+        return false;
+    }
+}
+
+function borang() {
+    let x = document.forms["myForms"]["alamat"].value;
+    if(x=="") {
+        alert("Alamat must be filled out");
+        return false;
+    }
+}
+
+function borang() {
+    let x = document.forms["myForms"]["bangsa"].value;
+    if(x=="") {
+        alert("Bangsa must be filled out");
+        return false;
+    }
 }
